0ade40db24fcdc2160556be4613a05398750e055
AC2407 <35344306+AC2407@users.noreply.github.com>
2018-01-16 18:59:37 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    if(n % 2 == 0)
+        printf("%d is even number", n);
+    else
+        printf("%d is odd number", n);
+}
